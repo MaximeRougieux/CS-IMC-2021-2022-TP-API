161fe8f2650cbfd033c371c3b583d566c4fb291c
MaximeRougieux <maxime.rougieux@student-cs.fr>
2022-01-22 20:00:13 +0100

    All queries

--- Query3/__init__.py
+++ Query3/__init__.py
@@ -1,7 +1,9 @@
 import logging
-import pyodbc as pyodbc
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
 import os
-
+import pyodbc as pyodbc
 import azure.functions as func
 
 
@@ -17,30 +19,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    server = os.environ["TPBDD_SERVER"]
-    database = os.environ["TPBDD_DB"]
-    username = os.environ["TPBDD_USERNAME"]
-    password = os.environ["TPBDD_PASSWORD"]
-    driver= '{ODBC Driver 17 for SQL Server}'
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
 
-    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 :
+    if len(neo4j_server) == 0 or len(neo4j_user) == 0 or len(neo4j_password) == 0:
         return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
-        
+
     errorMessage = ""
     dataString = ""
     try:
-        logging.info("Test de connexion avec pyodbc...")
-        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT genre, AVG(averageRating) FROM [dbo].[tGenres] LEFT JOIN [dbo].[tTitles] ON [dbo].[tGenres].tconst = [dbo].[tTitles].tconst WHERE averageRating >= 0 GROUP BY genre")
-            rows = cursor.fetchall()
-            for row in rows:
-                dataString += f"SQL: genre={row[0]}, average rating={row[1]}\n"
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        producers = graph.run(
+            "MATCH (t:Film)-[r:IS_OF_GENRE]->(g:Genre) RETURN g.genre,avg(t.averageRating)")
+        for producer in producers:
+            dataString += f"CYPHER: nconst={producer['n.nconst']}, primaryName={producer['n.primaryName']}\n"
+
     except:
-        errorMessage = "Erreur de connexion a la base SQL"
+        errorMessage = "Erreur de connexion a la base Neo4j"
 
     if name:
-            nameMessage = f"Hello, {name}!\n"
+        nameMessage = f"Hello, {name}!\n"
     else:
         nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
 
--- Query4/__init__.py
+++ Query4/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import pyodbc as pyodbc
+import os
 
 import azure.functions as func
 
@@ -15,10 +17,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver = '{ODBC Driver 17 for SQL Server}'
+
+    if len(server) == 0 or len(database) == 0 or len(username) == 0 or len(password) == 0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+
+    errorMessage = ""
+    dataString = ""
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD=' + password) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "SELECT genre, AVG(runtimeMinutes), STDEV(runtimeMinutes) FROM [dbo].[tGenres] LEFT JOIN [dbo].[tTitles] ON [dbo].[tGenres].tconst = [dbo].[tTitles].tconst WHERE runtimeMinutes >= 0 GROUP BY genre")
+            rows = cursor.fetchall()
+            for row in rows:
+                dataString += f"SQL: genre={row[0]}\n"
+    except:
+        errorMessage = "Erreur de connexion a la base SQL"
+
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        nameMessage = f"Hello, {name}!\n"
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
+
+    if errorMessage != "":
+        return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
+
+    else:
+        return func.HttpResponse(dataString + nameMessage + " Connexions réussies a Neo4j et SQL!")
